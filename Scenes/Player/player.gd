3067b91b566b664930232f1ec64a37aa516f3fb7
Jack@ <148102997+JackAt4@users.noreply.github.com>
2023-11-14 11:15:31 -0600

    Tilemaps (In progress)
    Still in progress

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,11 +1,25 @@
 extends CharacterBody2D
 
-@export var speed: int = 50
+@export var speed: int = 280
+@onready var animations = $AnimationPlayer
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed
+
+func updateAnimation():
+	if velocity.length() == 0:
+		if animations.is_playing():
+			animations.stop()
+	else:
+		var direction = "Down"
+		if velocity.x < 0: direction = "Left"
+		elif velocity.x > 0: direction = "Right"
+		elif velocity.y < 0: direction = "Up"
 	
-func _physics_process(delta):
+		animations.play("walk" + direction)
+
+func _physics_process(_delta):
 	handleInput()
 	move_and_slide()
+	updateAnimation()
